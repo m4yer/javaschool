a15dc40761162c2842bb3bf8aa82c7606c92bb4b
Mayer <aspid888@gmail.com>
2018-08-20 14:49:59 +0300

    #1DL: .

--- javaschool/src/test/java/integration/UserServiceIntegrationTest.java
+++ javaschool/src/test/java/integration/UserServiceIntegrationTest.java
@@ -3,7 +3,6 @@ package integration;
 import com.tsystems.dao.api.UserDAO;
 import com.tsystems.dto.UserDTO;
 import com.tsystems.entity.User;
-import com.tsystems.entity.converter.Converter;
 import com.tsystems.exceptions.RegisterFailedException;
 import com.tsystems.service.implementation.UserServiceImpl;
 import org.junit.Before;
@@ -11,12 +10,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
@@ -24,6 +22,8 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceIntegrationTest {
     private static final String TEST_BIRTHDAY = "24/10/1997";
+    private static final String TEST_USERNAME = "TEST_USER";
+    private static final String TEST_EMAIL = "aspid888@yahoo.com";
     private User user;
     private List<User> users;
 
@@ -36,17 +36,21 @@ public class UserServiceIntegrationTest {
     public void init() {
         users = new ArrayList<>();
         user = new User();
-        user.setUsername("TEST_USER");
+        user.setUsername(TEST_USERNAME);
         user.setPassword("testPassword");
-        user.setEmail("aspid888@yahoo.com");
+        user.setEmail(TEST_EMAIL);
         user.setFirstname("Maksim");
         user.setLastname("Ivanov");
+        users.add(user);
         when(userDAO.getAll()).thenReturn(users);
+        when(userDAO.findByUsername(TEST_USERNAME)).thenReturn(findByUsernameMock(TEST_USERNAME));
     }
 
     @Test
     public void addUserTest() {
         int usersListSizeBefore = userService.getAllUsers().size();
+        user.setEmail("test@test.ru");
+        user.setUsername("anotherusername");
         userService.addUser(user, TEST_BIRTHDAY);
         users.add(user);
         List<UserDTO> users = userService.getAllUsers();
@@ -55,8 +59,23 @@ public class UserServiceIntegrationTest {
 
     @Test(expected = RegisterFailedException.class)
     public void addUserWithInvalidData() {
-        user.setEmail("testemail");
         userService.addUser(user, TEST_BIRTHDAY);
+    }
+
+    @Test(expected = RegisterFailedException.class)
+    public void addUserWithInvalidData2() {
+        userService.addUser(user, TEST_BIRTHDAY);
+    }
+
+    @Test(expected = RegisterFailedException.class)
+    public void addUserWithAlreadyRegisteredUsername() {
+        users.add(user);
+        userService.addUser(user, TEST_BIRTHDAY);
+    }
+
+    private User findByUsernameMock(String username) {
+        Predicate<User> userPredicate = userElem -> userElem.getUsername().equals(username);
+        return users.stream().anyMatch(userPredicate) ? user : null;
     }
 
 }
